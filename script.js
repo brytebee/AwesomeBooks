787c10e6bc10e8ff83793a1a6287614e486ba4fd
Atsighi Bright <brytebee@gmail.com>
2021-08-09 14:13:06 +0100

    Add removeBook function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const books = [];
 
 function addNewBook(title, author) {
-  books.push({title, author});
+  books.push({ title, author });
+}
+
+function removeBook(title) {
+  books.filter(book => book.title === title);
 }
