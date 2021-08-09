3fdb8c95a3894693eb4a3813516d252d58fb115e
safafa <safaaballagh97@gmail.com>
2021-08-09 15:09:05 +0100

    display list of books

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const books = [];
+list = document.getElementById('booklist');
+console.log("Hello")
 
-function addNewBook(title, author) {
-  books.push({ title, author });
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+function addNewBook() {
+    title = document.getElementById('title');
+    author = document.getElementById('author');
+    book = new Book(title.value, author.value);
+    books.push(book);
+    displayBooks();
+    
 }
 
 function removeBook(title) {
   books.filter(book => book.title === title);
 }
+
+function displayBooks() {
+  books.map(book => {
+    let li = document.createElement("li");
+    li.innerHTML = book.title + " " + book.author;
+    list.appendChild(li);
+  })
+}
+
+
