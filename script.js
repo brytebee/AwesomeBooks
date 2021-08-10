47d8ea93f97f69318cddf139a19daf6af09e504a
Atsighi Bright <brytebee@gmail.com>
2021-08-10 10:03:53 +0100

    Fix linter errors on JS

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-var books = [];
-let store = window.localStorage;
-list = document.getElementById('booklist');
-
-function addNewBook() {
-    title = document.getElementById('title').value;
-    author = document.getElementById('author').value;
-    books.push({ title , author });
-    store.setItem('storeBook', JSON.stringify(books));
-    displayBooks();  
-}
+let books = [];
+const store = window.localStorage;
+let list = document.getElementById('booklist');
 
 function displayBooks() {
   while (list.firstChild) {
     list.removeChild(list.firstChild);
   }
-   books = JSON.parse(localStorage.getItem("storeBook"))
-   books.map(book => {
-    let li = document.createElement("li");
-    li.innerHTML = book.title + " " + book.author + " ";
-    let removeButton = document.createElement('button');
+  books = JSON.parse(localStorage.getItem("storeBook"));
+  books.map((book) => {
+    const li = document.createElement("li");
+    li.innerHTML = book.title + ' ' + book.author;
+    const removeButton = document.createElement('button');
     removeButton.innerHTML = 'Remove';
-    removeButton.addEventListener('click', (b) => {
-        books = books.filter(bok => bok.title !== book.title);
-        store.setItem('storeBook', JSON.stringify(books));
-        window.location.reload();
-    })  
+    removeButton.addEventListener('click', () => {
+      books = books.filter((bok) => bok.title !== book.title);
+      store.setItem('storeBook', JSON.stringify(books));
+      window.location.reload();
+    });
     li.appendChild(removeButton);
     list.appendChild(li);
-  })
+  });
 }
 
-window.addEventListener('load',(e) =>{
-    displayBooks();
-})
+function addNewBook() {
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  books.push({ title , author });
+  store.setItem('storeBook', JSON.stringify(books));
+  displayBooks();
+}
 
-
-
+window.addEventListener('load', () => {
+  displayBooks();
+});
