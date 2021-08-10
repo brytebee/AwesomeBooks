f5c6118d3ee6e9f2e90f1c61e1b91d54594ace93
safafa <safaaballagh97@gmail.com>
2021-08-10 09:48:55 +0100

    Add remove book function

--- script.js
+++ script.js
@@ -1,61 +1,38 @@
-const books = [];
+var books = [];
 let store = window.localStorage;
-console.log(store);
 list = document.getElementById('booklist');
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-}
-
 function addNewBook() {
-    title = document.getElementById('title');
-    author = document.getElementById('author');
-    book = new Book(title.value, author.value);
-    books.push(book);
+    title = document.getElementById('title').value;
+    author = document.getElementById('author').value;
+    books.push({ title , author });
     store.setItem('storeBook', JSON.stringify(books));
-    console.log(store.getItem('author'));
     displayBooks();  
-}
-
-function removeBook(title) {
-  books.filter(book => book.title === title);
-  // location.reload();
 }
 
 function displayBooks() {
   while (list.firstChild) {
     list.removeChild(list.firstChild);
   }
-
-  console.log(typeof(store.getItem('storeBook')));
-
-  JSON.parse(localStorage.getItem("storeBook")).map(book => {
+   books = JSON.parse(localStorage.getItem("storeBook"))
+   books.map(book => {
     let li = document.createElement("li");
-    li.innerHTML = book.title + " " + book.author;
+    li.innerHTML = book.title + " " + book.author + " ";
     let removeButton = document.createElement('button');
     removeButton.innerHTML = 'Remove';
-    removeButton.onclick = removeBook(book.title);
+    removeButton.addEventListener('click', (b) => {
+        books = books.filter(bok => bok.title !== book.title);
+        store.setItem('storeBook', JSON.stringify(books));
+        window.location.reload();
+    })  
     li.appendChild(removeButton);
     list.appendChild(li);
   })
 }
 
-function storebooks(books) {
-  // Get the existing data
-	var existing = localStorage.getItem('storeBook');
-  console.log(existing);
-	// If no existing data, create an array
-	// Otherwise, convert the localStorage string to an array
-	existing = existing ? existing.split(',') : [];
-
-	// Add new data to localStorage Array
-	existing = books;
-
-	// Save back to localStorage
-	localStorage.setItem('storeBook', existing.toString());
-}
-
+window.addEventListener('load',(e) =>{
+    displayBooks();
+})
 
 
 
