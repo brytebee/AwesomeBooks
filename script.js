981f5ce625ee5d96f48562e7aca1698ebda3b1e2
Atsighi Bright <brytebee@gmail.com>
2021-08-10 13:58:22 +0100

    Use classes

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
-let books = [];
-const store = window.localStorage;
-const list = document.getElementById('booklist');
-const form = document.getElementById('form');
-
-function displayBooks() {
-  while (list.firstChild) {
-    list.removeChild(list.firstChild);
+class Library {
+  
+  constructor(books) {
+    this.books = books;
+    this.store = window.localStorage;
+    this.list = document.getElementById('booklist');
+    this.form = document.getElementById('form');
   }
-  books = JSON.parse(localStorage.getItem('storeBook'));
-  books.forEach((book) => {
-    const li = document.createElement('li');
-    li.innerHTML = `author: ${book.author}: title: ${book.title}`;
-    const removeButton = document.createElement('button');
-    removeButton.innerHTML = 'Remove';
-    removeButton.addEventListener('click', () => {
-      books = books.filter((bok) => bok.title !== book.title);
-      store.setItem('storeBook', JSON.stringify(books));
-      window.location.reload();
+
+  displayBooks() {
+    while (this.list.firstChild) {
+      this.list.removeChild(this.list.firstChild);
+    }
+    this.books = JSON.parse(localStorage.getItem('storeBook'));
+    this.books.forEach((book) => {
+      const li = document.createElement('li');
+      li.innerHTML = `author: ${book.author}: title: ${book.title}`;
+      const removeButton = document.createElement('button');
+      removeButton.innerHTML = 'Remove';
+      removeButton.addEventListener('click', () => {
+        this.books = this.books.filter((bok) => bok.title !== book.title);
+        this.store.setItem('storeBook', JSON.stringify(this.books));
+        window.location.reload();
+      });
+      li.appendChild(removeButton);
+      this.list.appendChild(li);
     });
-    li.appendChild(removeButton);
-    list.appendChild(li);
-  });
+    console.log('Hello');
+  }
+  
+  addBook(){
+    this.books.push({ title, author });
+    this.store.setItem('storeBook', JSON.stringify(this.books));
+    this.displayBooks();
+  }
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  books.push({ title, author });
-  store.setItem('storeBook', JSON.stringify(books));
-  displayBooks();
-});
+const library = new Library(JSON.parse(localStorage.getItem('storeBook')));
 
 window.addEventListener('load', () => {
   if (JSON.parse(localStorage.getItem('storeBook')) === null) {
     store.setItem('storeBook', JSON.stringify(books));
+    library.displayBooks();
+  } else {
+    library.displayBooks();
   }
-  displayBooks();
+});
+
+document.getElementById('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  library.addBook(title, author);
 });
