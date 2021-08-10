0868ec0c70411ba98e1ad7f20e0f7e87a0659e9a
Atsighi Bright <brytebee@gmail.com>
2021-08-10 14:14:50 +0100

    fix errors in script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 class Library {
-  
   constructor(books) {
     this.books = books;
     this.store = window.localStorage;
@@ -17,25 +16,24 @@ class Library {
       li.innerHTML = `author: ${book.author}: title: ${book.title}`;
       const removeButton = document.createElement('button');
       removeButton.innerHTML = 'Remove';
-      removeButton.addEventListener('click', ()=> {
-        this.removeBook(book.title)
+      removeButton.addEventListener('click', () => {
+        this.removeBook(book.title);
         window.location.reload();
       });
       li.appendChild(removeButton);
       this.list.appendChild(li);
     });
   }
-  
-  addBook(title, author){
+
+  addBook(title, author) {
     this.books.push({ title, author });
     this.store.setItem('storeBook', JSON.stringify(this.books));
     this.displayBooks();
   }
 
-  removeBook(title){
+  removeBook(title) {
     this.books = this.books.filter((bok) => bok.title !== title);
     this.store.setItem('storeBook', JSON.stringify(this.books));
-    
   }
 }
 
@@ -43,7 +41,7 @@ const library = new Library(JSON.parse(localStorage.getItem('storeBook')));
 
 window.addEventListener('load', () => {
   if (JSON.parse(localStorage.getItem('storeBook')) === null) {
-    store.setItem('storeBook', JSON.stringify(books));
+    window.localStorage.setItem('storeBook', JSON.stringify([]));
     library.displayBooks();
   } else {
     library.displayBooks();
